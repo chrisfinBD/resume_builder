8503b82bd2c7d749d6551553b2e76fe1ec1eaaea
Christie Finnie <christie.finnie@gmail.com>
2019-12-06 23:14:41 -0800

    Add test page. Fill work experience.

--- submit.php
+++ submit.php
@@ -1,5 +1,43 @@
 <?php
 var_dump($_POST);
 
-
+echo $_POST["emailField"];
 ?>
+
+
+<html>
+    <body>
+        <h1><?php echo $_POST["nameField"] ?></h1>
+        <p>
+            <?php echo $_POST["emailField"] ?> - 
+            <a href="<?php echo $_POST["githubField"] ?>"><?php echo $_POST["githubField"] ?></a>
+        </p>
+
+        <hr>
+
+
+        <h2>Work Experience</h2>
+        <p> 
+            <i><?php echo $_POST["jobTitle"] ?></i> - <?php echo $_POST["companyName"] ?>
+        </p>
+        <p>
+            <?php echo $_POST["startDate"] ?> - <?php echo $_POST["endDate"] ?>
+        
+        </p>
+
+        
+            <?php 
+                $duty_array = [];
+                $duties = $_POST["duties"];
+                if($duties != ""){
+                    $duty_array = explode("\n", $duties);
+                }
+            ?>
+         <ul>  
+                <?php  foreach($duty_array as $line) { ?>
+                    <li><?php echo $line; ?></li>
+                <?php } ?>
+            </ul>
+
+    </body>
+</html>
--- test.php
+++ test.php
@@ -7,16 +7,16 @@
     <body>
         <form action="./submit.php" method="POST">
                 <label for="nameField">Name</label>
-                <input type="text" class="field" name="nameField" id="nameField" placeholder="Jane Doe" />
+                <input type="text" class="field" name="nameField" id="nameField" value="Jane Doe" />
                 <br />
                 <label for="emailField">Email</label>
-                <input type="email" class="field" name="emailField" id="emailField" placeholder="j.doe@example.com" />
+                <input type="email" class="field" name="emailField" id="emailField" value="j.doe@example.com" />
                 <br />
                 <label for="phoneField">Phone</label>
-                <input type="text" class="field" name="phoneField" id="phoneField" placeholder="777-777-7777" />
+                <input type="text" class="field" name="phoneField" id="phoneField" value="777-777-7777" />
                 <br />
                 <label for="githubField">Github URL</label>
-                <input type="url" class="field" name="githubField" id="githubField" placeholder="https://github.com/JDWorks" />
+                <input type="url" class="field" name="githubField" id="githubField" value="https://github.com/JDWorks" />
                 <br />
                 <label for="eduField">Highest Education</label>
                 <select class="field" name="eduField" id="eduField">
@@ -31,19 +31,26 @@
 
                 <h3>Job History</h3>
                 <label for="jobTitle">Job Title</label>
-                <input type="text" class="field" name="jobTitle" id="jobTitle" placeholder="Title of Job Level 1" />
+                <input type="text" class="field" name="jobTitle" id="jobTitle" value="Title of Job Level 1" />
+                <br />
+
+                <label for="companyName">Company</label>
+                <input type="text" class="field" name="companyName" id="companyName" value="Company Inc." />
                 <br />
 
                 <label for="startDate">Start Date (Month and Year)</label>
-                <input type="month" class="field" name="startDate" id="startDate" />
+                <input type="month" class="field" name="startDate" id="startDate" value="2019-12" />
                 <br />
 
                 <label for="endDate">End Date (Month and Year)</label>
-                <input type="month" class="field" name="endDate" id="endDate" />
+                <input type="month" class="field" name="endDate" id="endDate" value="2019-12" />
                 <br />
 
                 <label for="duties">Duties</label>
-                <textarea class="field" name="duties" id="duties" rows="4" cols="50"></textarea>
+                <textarea class="field" name="duties" id="duties" rows="4" cols="50">task 1
+task 2
+task 3
+                </textarea>
                 <p>Separate each duty on a new line.</p>
                 <br />
 
